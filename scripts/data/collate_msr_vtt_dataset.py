642de62e18c684495d017f9f5a5d31bd9034f364
FrankLeeeee <somerlee.9@gmail.com>
2024-02-23 11:24:06 +0800

    added dataset processing scripts

--- scripts/data/collate_msr_vtt_dataset.py
+++ scripts/data/collate_msr_vtt_dataset.py
@@ -1,14 +1,15 @@
 import argparse
 import json
-import os
-from typing import Dict, Tuple
-import warnings
-from tqdm import tqdm
-import shutil
 import multiprocessing
+import os
+import shutil
+import warnings
+from typing import Dict, Tuple
 
+from tqdm import tqdm
 
 DEFAULT_TYPES = ["train", "val", "test"]
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -34,9 +35,10 @@ def get_annotations(root_path: str):
         root_path (str): The root path to the MSR-VTT dataset
     """
     annotation_json_file = os.path.join(root_path, "annotation/MSR_VTT.json")
-    with open(annotation_json_file, 'r') as f:
+    with open(annotation_json_file, "r") as f:
         data = json.load(f)
     return data
+
 
 def get_video_list(root_path: str, dataset_type: str):
     """
@@ -48,7 +50,7 @@ def get_video_list(root_path: str, dataset_type: str):
     """
     assert dataset_type in DEFAULT_TYPES, f"Expected the dataset type to be in {DEFAULT_TYPES}, but got {dataset_type}"
     dataset_file_path = os.path.join(root_path, f"structured-symlinks/{dataset_type}_list_full.txt")
-    with open(dataset_file_path, 'r') as f:
+    with open(dataset_file_path, "r") as f:
         video_list = f.readlines()
         video_list = [x.strip() for x in video_list]
     return video_list
@@ -67,7 +69,7 @@ def copy_video(video_id: str, root_path: str, output_path: str, dataset_type: st
     assert dataset_type in DEFAULT_TYPES, f"Expected the dataset type to be in {DEFAULT_TYPES}, but got {dataset_type}"
     src_file = os.path.join(root_path, f"videos/all/{video_id}.mp4")
     dst_folder = os.path.join(output_path, f"{dataset_type}/videos")
-    dst_file = os.path.join(dst_folder, f'{video_id}.mp4')
+    dst_file = os.path.join(dst_folder, f"{video_id}.mp4")
     os.makedirs(dst_folder, exist_ok=True)
 
     # create symlink
@@ -75,15 +77,17 @@ def copy_video(video_id: str, root_path: str, output_path: str, dataset_type: st
     if not os.path.islink(dst_file):
         shutil.copy(src_file, dst_file)
 
+
 def get_annotation_file_path(output_path: str, dataset_type: str):
     file_path = os.path.join(output_path, f"{dataset_type}/annotations.json")
     return file_path
 
+
 def collate_annotation_files(
-        annotations: Dict, 
-        root_path: str, 
-        output_path: str,
-        ):
+    annotations: Dict,
+    root_path: str,
+    output_path: str,
+):
     """
     Collate the video and caption data into a single folder.
 
@@ -104,14 +108,11 @@ def collate_annotation_files(
 
     print("Collating annotations files")
 
-    for anno in tqdm(annotations['annotations']):
-        video_id = anno['image_id']
-        caption = anno['caption']
+    for anno in tqdm(annotations["annotations"]):
+        video_id = anno["image_id"]
+        caption = anno["caption"]
 
-        obj = {
-            "file": f"{video_id}.mp4",
-            "captions": [caption]
-        }
+        obj = {"file": f"{video_id}.mp4", "captions": [caption]}
 
         if video_id in train_video_list:
             collated_train_data.append(obj)
@@ -121,20 +122,22 @@ def collate_annotation_files(
             collated_test_data.append(obj)
         else:
             warnings.warn(f"Video {video_id} not found in any of the dataset splits")
-    
+
     def _save_caption_files(obj, dataset_type):
         dst_file = get_annotation_file_path(output_path, dataset_type)
         os.makedirs(os.path.dirname(dst_file), exist_ok=True)
-        with open(dst_file, 'w') as f:
+        with open(dst_file, "w") as f:
             json.dump(obj, f, indent=4)
-        
+
     _save_caption_files(collated_train_data, "train")
     _save_caption_files(collated_val_data, "val")
     _save_caption_files(collated_test_data, "test")
 
+
 def copy_file(path_pair: Tuple[str, str]):
     src_path, dst_path = path_pair
     shutil.copyfile(src_path, dst_path)
+
 
 def copy_videos(root_path: str, output_path: str, num_workers: int = 8):
     """
@@ -153,22 +156,20 @@ def copy_videos(root_path: str, output_path: str, num_workers: int = 8):
         output_video_folder_path = os.path.join(output_path, f"{dataset_type}/videos")
         os.makedirs(output_video_folder_path, exist_ok=True)
 
-        with open(annotation_file_path, 'r') as f:
+        with open(annotation_file_path, "r") as f:
             annotation_data = json.load(f)
-        
-        video_ids = [obj['file'] for obj in annotation_data]
+
+        video_ids = [obj["file"] for obj in annotation_data]
         unique_video_ids = list(set(video_ids))
 
         path_pairs = [
-            (
-                os.path.join(root_path, f"videos/all/{video_id}"),
-                os.path.join(output_video_folder_path, video_id)
-            )
+            (os.path.join(root_path, f"videos/all/{video_id}"), os.path.join(output_video_folder_path, video_id))
             for video_id in unique_video_ids
         ]
 
         for _ in tqdm(pool.imap_unordered(copy_file, path_pairs), total=len(path_pairs)):
             pass
+
 
 def main():
     args = parse_args()
