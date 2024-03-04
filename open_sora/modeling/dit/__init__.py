811a4a1f78ffe840851598f65b8661e79eb0c829
ver217 <lhx0217@gmail.com>
2024-03-04 14:24:48 +0800

    [hotfix] update benchmark for ddp

--- open_sora/modeling/dit/__init__.py
+++ open_sora/modeling/dit/__init__.py
@@ -1,3 +1,3 @@
-from .dit import DiT, DiT_models
+from .dit import SUPPORTED_SEQ_PARALLEL_MODES, DiT, DiT_models
 
-__all__ = ["DiT_models", "DiT"]
+__all__ = ["DiT_models", "DiT", "SUPPORTED_SEQ_PARALLEL_MODES"]
