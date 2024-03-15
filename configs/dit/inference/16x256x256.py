c3741c596b4dc17c62f62b8061479d4c6aaf6e14
FrankLeeeee <somerlee.9@gmail.com>
2024-03-15 21:52:48 +0800

    added configs and tests

---
+++ configs/dit/inference/16x256x256.py
@@ -0,0 +1,32 @@
+# sample size
+num_frames = 16
+fps = 8
+image_size = (256, 256)
+
+# model config
+model = dict(
+    type="DiT-XL/2",
+    condition="text",
+    from_pretrained="YOUR_MODEL_PATH",
+)
+vae = dict(
+    type="VideoAutoencoderKL",
+    from_pretrained="stabilityai/sd-vae-ft-ema",
+)
+text_encoder = dict(
+    type="clip",
+    from_pretrained="openai/clip-vit-base-patch32",
+    model_max_length=77,
+)
+scheduler = dict(
+    type="dpm-solver",
+    num_sampling_steps=20,
+    cfg_scale=4.0,
+)
+dtype = "fp16"
+
+# prompts
+batch_size = 2
+seed = 42
+prompt_path = "./assets/texts/ucf101_labels.txt"
+save_dir = "./samples/"
