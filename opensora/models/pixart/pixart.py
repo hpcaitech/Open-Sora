6a4b9add90379c8d6678625468b437e6ac97129d
Frank Lee <somerlee.9@gmail.com>
2024-03-17 21:59:37 +0800

    added licenses (#80)

--- opensora/models/pixart/pixart.py
+++ opensora/models/pixart/pixart.py
@@ -1,5 +1,18 @@
 # Adapted from PixArt
 #
+# Copyright (C) 2023  PixArt-alpha/PixArt-alpha
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+#
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 # --------------------------------------------------------
