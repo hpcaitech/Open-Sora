233fe5b4c20373d1937786650faada21ac717d54
FrankLeeeee <somerlee.9@gmail.com>
2024-03-17 21:57:31 +0800

    added licenses

--- opensora/models/latte/latte.py
+++ opensora/models/latte/latte.py
@@ -1,12 +1,24 @@
-# Adapted from Latte
+# Copyright 2024 Vchitect/Latte
 #
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-# --------------------------------------------------------
-# References:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.# Modified from Latte
+#
+#
+# This file is mofied from https://github.com/Vchitect/Latte/blob/main/models/latte.py
+#
+# With references to:
 # Latte:  https://github.com/Vchitect/Latte
 # DiT:    https://github.com/facebookresearch/DiT/tree/main
-# --------------------------------------------------------
+
 
 import torch
 from einops import rearrange, repeat
