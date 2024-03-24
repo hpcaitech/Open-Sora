7d478f509441b92672c28bfb01a44fbea5a8edf5
Zangwei Zheng <zangwei@comp.nus.edu.sg>
2024-03-24 15:09:42 +0800

    complete masked demo

--- configs/opensora/inference-long/16x512x512-extend.py
+++ configs/opensora/inference-long/16x512x512-extend.py
@@ -34,13 +34,14 @@ dtype = "fp16"
 # Condition
 prompt_path = None
 prompt = [
+    "Drone view of waves crashing against the rugged cliffs along Big Sur’s garay point beach. The crashing blue waters create white-tipped waves, while the golden light of the setting sun illuminates the rocky shore. A small island with a lighthouse sits in the distance, and green shrubbery covers the cliff’s edge. The steep drop from the road down to the beach is a dramatic feat, with the cliff's edges jutting out over the sea. This is a view that captures the raw beauty of the coast and the rugged landscape of the Pacific Coast Highway.",
     "In an ornate, historical hall, a massive tidal wave peaks and begins to crash. Two surfers, seizing the moment, skillfully navigate the face of the wave."
 ]
 
-loop = 5
+loop = 10
 condition_frame_length = 4
-reference_path = ["assets/images/condition/wave.png"]
-mask_strategy = ["0,0,0,1,0"]  # valid when reference_path is not None
+reference_path = ["assets/images/condition/cliff.png", "assets/images/condition/wave.png"]
+mask_strategy = ["0,0,0,1,0", "0,0,0,1,0"]  # valid when reference_path is not None
 # (loop id, ref id, ref start, length, target start)
 
 # Others
