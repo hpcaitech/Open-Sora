66cbf1688fb28f07877329620212f3a1d67f3385
Zangwei Zheng <zangwei@comp.nus.edu.sg>
2024-03-26 17:32:15 +0800

    refactor temporal_random_crop

--- opensora/datasets/video_transforms.py
+++ opensora/datasets/video_transforms.py
@@ -20,7 +20,6 @@ import random
 
 import numpy as np
 import torch
-from PIL import Image
 
 
 def _is_tensor_video_clip(clip):
@@ -31,23 +30,6 @@ def _is_tensor_video_clip(clip):
         raise ValueError("clip should be 4D. Got %dD" % clip.dim())
 
     return True
-
-
-def center_crop_arr(pil_image, image_size):
-    """
-    Center cropping implementation from ADM.
-    https://github.com/openai/guided-diffusion/blob/8fb3ad9197f16bbc40620447b2742e13458d2831/guided_diffusion/image_datasets.py#L126
-    """
-    while min(*pil_image.size) >= 2 * image_size:
-        pil_image = pil_image.resize(tuple(x // 2 for x in pil_image.size), resample=Image.BOX)
-
-    scale = image_size / min(*pil_image.size)
-    pil_image = pil_image.resize(tuple(round(x * scale) for x in pil_image.size), resample=Image.BICUBIC)
-
-    arr = np.array(pil_image)
-    crop_y = (arr.shape[0] - image_size) // 2
-    crop_x = (arr.shape[1] - image_size) // 2
-    return Image.fromarray(arr[crop_y : crop_y + image_size, crop_x : crop_x + image_size])
 
 
 def crop(clip, i, j, h, w):
