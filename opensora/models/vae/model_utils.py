953edfc32dab0b1709062d0902b44ca50db3927a
shenchenhui <shenchenhui@shenchenhuis-MacBook-Pro.local>
2024-03-28 23:08:32 +0800

    debug

--- opensora/models/vae/model_utils.py
+++ opensora/models/vae/model_utils.py
@@ -81,7 +81,9 @@ class DiagonalGaussianDistribution(object):
         self, 
         parameters, 
         deterministic=False,
+        dtype=torch.bfloat16,
     ):
+        self.dtype = dtype
         self.parameters = parameters
         self.mean, self.logvar = torch.chunk(parameters, 2, dim=1) # SCH: channels dim
         self.logvar = torch.clamp(self.logvar, -30.0, 20.0)
@@ -89,10 +91,10 @@ class DiagonalGaussianDistribution(object):
         self.std = torch.exp(0.5 * self.logvar)
         self.var = torch.exp(self.logvar)
         if self.deterministic:
-            self.var = self.std = torch.zeros_like(self.mean).to(device=self.parameters.device)
+            self.var = self.std = torch.zeros_like(self.mean).to(device=self.parameters.device, dtype=dtype)
 
     def sample(self):
-        x = self.mean + self.std * torch.randn(self.mean.shape).to(device=self.parameters.device)
+        x = self.mean + self.std * torch.randn(self.mean.shape).to(device=self.parameters.device, dtype=self.dtype)
         return x
 
     def kl(self, other=None):
