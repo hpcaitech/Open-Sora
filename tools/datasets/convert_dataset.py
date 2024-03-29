59443ce58f24e564d9f05051e69e78b3ebc13827
Zheng Zangwei (Alex Zheng) <zangwei@comp.nus.edu.sg>
2024-03-29 23:34:10 +0800

    Update image process (#5)
    * [docs] update tool docs
    
    * update aes

--- tools/datasets/convert_dataset.py
+++ tools/datasets/convert_dataset.py
@@ -6,7 +6,7 @@ from torchvision.datasets import ImageNet
 
 
 IMG_EXTENSIONS = (".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm", ".tif", ".tiff", ".webp")
-VID_EXTENSIONS = ("mp4", "avi", "mov", "mkv")
+VID_EXTENSIONS = (".mp4", ".avi", ".mov", ".mkv")
 
 
 def get_filelist(file_path, exts=None):
