d8666a73c41ef5024df68e5c0a6a1fbcb4ccb151
Shen-Chenhui <e0052680@u.nus.edu>
2024-04-09 18:04:08 +0800

    register module

--- opensora/models/vae/__init__.py
+++ opensora/models/vae/__init__.py
@@ -1,3 +1,4 @@
 from .vae import VideoAutoencoderKL, VideoAutoencoderKLTemporalDecoder
-from .vae_3d import VAE_3D
+from .vae_3d import VAE_3D, VAE_3D_B
+from .vae_3d_v2 import VAE_3D_V2, VAE_MAGVIT_V2
 
