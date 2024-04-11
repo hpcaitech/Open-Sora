42d7290c361df6acfd45206898b7f67bd2a35f4b
Shen-Chenhui <e0052680@u.nus.edu>
2024-04-11 10:50:23 +0800

    add image encoding support

--- opensora/models/vae/vae_3d_v2.py
+++ opensora/models/vae/vae_3d_v2.py
@@ -8,7 +8,7 @@ from numpy import typing as nptyping
 from opensora.models.vae import model_utils 
 from opensora.registry import MODELS
 from opensora.utils.ckpt_utils import load_checkpoint
-from einops import rearrange
+from einops import rearrange, pack, unpack
 import torch.nn.functional as F
 
 """Encoder and Decoder stuctures with 3D CNNs."""
@@ -32,6 +32,16 @@ def divisible_by(num, den):
 
 def is_odd(n):
     return not divisible_by(n, 2)
+
+def pad_at_dim(t, pad, dim = -1, value = 0.):
+    dims_from_right = (- dim - 1) if dim < 0 else (t.ndim - dim - 1)
+    zeros = ((0, 0) * dims_from_right)
+    return F.pad(t, (*zeros, *pad), value = value)
+
+def SameConv2d(dim_in, dim_out, kernel_size):
+    kernel_size = cast_tuple(kernel_size, 2)
+    padding = [k // 2 for k in kernel_size]
+    return nn.Conv2d(dim_in, dim_out, kernel_size = kernel_size, padding = padding)
 
 class CausalConv3d(nn.Module):
     def __init__(
@@ -172,7 +182,8 @@ class Encoder(nn.Module):
             device=device,
         )
         
-        self.conv1 = self.conv_fn(in_out_channels, self.filters, kernel_size=(3, 3, 3), bias=False)
+        # NOTE: moved to VAE for separate first frame processing
+        # self.conv1 = self.conv_fn(in_out_channels, self.filters, kernel_size=(3, 3, 3), bias=False)
 
         # ResBlocks and conv downsample
         self.block_res_blocks = []
@@ -210,7 +221,10 @@ class Encoder(nn.Module):
 
     def forward(self, x):
         # dtype, device = x.dtype, x.device
-        x = self.conv1(x)
+
+        # NOTE: moved to VAE for separate first frame processing
+        # x = self.conv1(x)
+
         for i in range(self.num_blocks):
             for j in range(self.num_res_blocks):
                 x = self.block_res_blocks[i][j](x)
@@ -323,7 +337,9 @@ class Decoder(nn.Module):
                 )
                 
         self.norm1 = nn.GroupNorm(self.num_groups, prev_filters, device=device, dtype=dtype)
-        self.conv2 = self.conv_fn(prev_filters, self.output_dim, kernel_size=(3, 3, 3))
+
+        # NOTE: moved to VAE for separate first frame processing
+        # self.conv2 = self.conv_fn(prev_filters, self.output_dim, kernel_size=(3, 3, 3))
 
 
     def forward(
@@ -348,7 +364,8 @@ class Decoder(nn.Module):
 
         x = self.norm1(x)
         x = self.activate(x)
-        x = self.conv2(x)
+        # NOTE: moved to VAE for separate first frame processing
+        # x = self.conv2(x) 
         return x
     
 
@@ -358,8 +375,10 @@ class VAE_3D_V2(nn.Module):
     def __init__(
         self, 
         latent_embed_dim = 256,
-        filters = 64,
+        filters = 128,
         num_res_blocks = 2,
+        image_size = (128, 128),
+        separate_first_frame_encoding = False,
         channel_multipliers = (1, 2, 2, 4),
         temporal_downsample = (True, True, False),
         num_groups = 32, # for nn.GroupNorm
@@ -382,8 +401,25 @@ class VAE_3D_V2(nn.Module):
                 dtype = torch.float16
             else:
                 raise NotImplementedError(f'dtype: {dtype}')
+            
 
-        # Model Initialization 
+        # ==== Model Params ====
+        self.image_size = cast_tuple(image_size, 2)
+        self.time_downsample_factor = 2**sum(temporal_downsample)
+        self.time_padding = self.time_downsample_factor - 1
+        self.separate_first_frame_encoding = separate_first_frame_encoding
+
+        # ==== Model Initialization ====
+
+        # encoder & decoder first and last conv layer
+        # SCH: NOTE: following MAGVIT, conv in bias=False in encoder first conv 
+        self.conv_in = CausalConv3d(in_out_channels, filters, kernel_size=(3, 3, 3), bias=False, dtype=dtype, device=device)
+        self.conv_in_first_frame = nn.Identity()
+        self.conv_out_first_frame = nn.Identity()
+        if separate_first_frame_encoding:
+            self.conv_in_first_frame = SameConv2d(in_out_channels, filters, (3,3))
+            self.conv_out_first_frame = SameConv2d(filters, in_out_channels, (3,3))
+        self.conv_out = CausalConv3d(filters, in_out_channels, (3,3), dtype=dtype, device=device)
 
 
         self.encoder = Encoder(
@@ -415,8 +451,8 @@ class VAE_3D_V2(nn.Module):
             dtype=dtype,
         )
 
-        self.quant_conv = nn.Conv3d(latent_embed_dim, 2*kl_embed_dim, 1)
-        self.post_quant_conv = nn.Conv3d(kl_embed_dim, latent_embed_dim, 1)
+        self.quant_conv = nn.Conv3d(latent_embed_dim, 2*kl_embed_dim, 1, device=device, dtype=dtype)
+        self.post_quant_conv = nn.Conv3d(kl_embed_dim, latent_embed_dim, 1, device=device, dtype=dtype)
         
         image_down = 2 ** len(temporal_downsample)
         t_down = 2 ** len([x for x in temporal_downsample if x == True])
@@ -430,34 +466,99 @@ class VAE_3D_V2(nn.Module):
     
     def encode(
         self,
-        x,
+        video,
+        video_contains_first_frame = True,
     ):
-        encoded_feature = self.encoder(x)
-        moments = self.quant_conv(encoded_feature).to(x.dtype)
+        encode_first_frame_separately = self.separate_first_frame_encoding and video_contains_first_frame
+
+        # whether to pad video or not
+        if video_contains_first_frame:
+            video_len = video.shape[2]
+            video = pad_at_dim(video, (self.time_padding, 0), value = 0., dim = 2)
+            video_packed_shape = [torch.Size([self.time_padding]), torch.Size([]), torch.Size([video_len - 1])]
+
+        # NOTE: moved encoder conv1 here for separate first frame encoding
+        if encode_first_frame_separately:
+            pad, first_frame, video = unpack(video, video_packed_shape, 'b c * h w')
+            first_frame = self.conv_in_first_frame(first_frame)
+        video = self.conv_in(video)
+        if encode_first_frame_separately:
+            video, _ = pack([first_frame, video], 'b c * h w')
+            video = pad_at_dim(video, (self.time_padding, 0), dim = 2)
+
+        encoded_feature = self.encoder(video)
+
+        moments = self.quant_conv(encoded_feature).to(video.dtype)
         posterior = model_utils.DiagonalGaussianDistribution(moments)
         return posterior
     
     def decode(
         self,
         z,
+        video_contains_first_frame = True,
     ):  
-        dtype = z.dtype
-        z = self.post_quant_conv(z).to(dtype)
+        # dtype = z.dtype
+        decode_first_frame_separately = self.separate_first_frame_encoding and video_contains_first_frame
+
+        z = self.post_quant_conv(z)
         dec = self.decoder(z)
-        return dec
+
+        # SCH: moved decoder last conv layer here for separate first frame decoding
+        if decode_first_frame_separately:
+            left_pad, dec_ff, dec = dec[:, :, :self.time_padding], dec[:, :, self.time_padding], dec[:, :, (self.time_padding + 1):]
+            out = self.conv_out(dec)
+            outff = self.conv_out_first_frame(dec_ff)
+            video, _ = pack([outff, out], 'b c * h w')
+        else:
+            video = self.conv_out(dec)
+            # if video were padded, remove padding
+            if video_contains_first_frame:
+                video = video[:, :, self.time_padding:]
+
+        return video
     
     def forward(
         self,
         input,
         sample_posterior=True,
+        video_contains_first_frame = True,
     ):  
-        posterior = self.encode(input)
+        assert input.ndim in {4, 5}, f"received input of {input.ndim} dimensions" # either image or video
+        assert input.shape[-2:] == self.image_size, f"received input size {input.shape[-2:]}, but config image size is {self.image_size}"
+
+        is_image = input.ndim == 4
+
+        if is_image:
+            video = rearrange(input, 'b c ... -> b c 1 ...')
+            video_contains_first_frame = True
+        else:
+            video = input
+
+        batch, channels, frames = video.shape[:3]
+        assert divisible_by(frames - int(video_contains_first_frame), self.time_downsample_factor), f'number of frames {frames} minus the first frame ({frames - int(video_contains_first_frame)}) must be divisible by the total downsample factor across time {self.time_downsample_factor}'
+
+        posterior = self.encode(
+            video,
+            video_contains_first_frame = video_contains_first_frame,
+        )
+
         if sample_posterior:
             z = posterior.sample()
         else:
             z = posterior.mode()
-        dec = self.decode(z)
-        return dec, posterior
+
+        recon_video = self.decode(
+            z, 
+            video_contains_first_frame = video_contains_first_frame
+        )
+
+        recon_loss = F.mse_loss(video, recon_video)
+
+        # TODO: add perceptual loss
+
+        # TODO: add adversarial loss
+
+        return recon_loss, recon_video
     
 
 @MODELS.register_module("VAE_MAGVIT_V2")
