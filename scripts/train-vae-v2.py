31ab38fedadd3246e4075d88098ace718929412b
Shen-Chenhui <e0052680@u.nus.edu>
2024-04-12 17:57:35 +0800

    gan training

--- scripts/train-vae-v2.py
+++ scripts/train-vae-v2.py
@@ -34,7 +34,7 @@ from opensora.utils.config_utils import (
 )
 from opensora.utils.misc import all_reduce_mean, format_numel_str, get_model_numel, requires_grad, to_torch_dtype
 from opensora.utils.train_utils import update_ema, MaskGenerator
-from opensora.models.vae.model_utils import VEA3DLoss
+from opensora.models.vae.vae_3d_v2 import VEALoss, DiscriminatorLoss, AdversarialLoss, pad_at_dim
 
 
 def main():
@@ -137,10 +137,16 @@ def main():
     logger.info(
         f"Trainable vae params: {format_numel_str(vae_numel_trainable)}, Total model params: {format_numel_str(vae_numel)}"
     )
-
+    
+    discriminator = build_module(cfg.discriminator, MODELS, device=device)
+    discriminator_numel, discriminator_numel_trainable = get_model_numel(discriminator)
+    logger.info(
+        f"Trainable discriminator params: {format_numel_str(discriminator_numel_trainable)}, Total model params: {format_numel_str(discriminator_numel)}"
+    )
 
     # 4.3. move to device
     vae = vae.to(device, dtype)
+    discriminator = discriminator.to(device, dtype)
 
     breakpoint() # TODO: check vae parameters
 
@@ -152,14 +158,16 @@ def main():
     lr_scheduler = None
     # disc optimizer
     disc_optimizer = HybridAdam(
-        filter(lambda p: p.requires_grad, vae.disc_parameters()), lr=cfg.lr, weight_decay=0, adamw_mode=True
+        filter(lambda p: p.requires_grad, discriminator.parameters()), lr=cfg.lr, weight_decay=0, adamw_mode=True
     )
     disc_lr_scheduler = None
 
     # 4.6. prepare for training
     if cfg.grad_checkpoint:
         set_grad_checkpoint(vae)
+        set_grad_checkpoint(discriminator)
     vae.train()
+    discriminator.train()
 
 
     # =======================================================
@@ -169,15 +177,16 @@ def main():
     vae, optimizer, _, dataloader, lr_scheduler = booster.boost(
         model=vae, optimizer=optimizer, lr_scheduler=lr_scheduler, dataloader=dataloader
     )
-    # TODO: do I need to pass the other parameters?
-    # TODO: can we use the same booster.boost?
-    vae.discriminator, disc_optimizer, _, _, disc_lr_scheduler = booster.boost(
-        model=vae.discriminator, optimizer=disc_optimizer, lr_scheduler=disc_lr_scheduler
-    )
-
     torch.set_default_dtype(torch.float)
     num_steps_per_epoch = len(dataloader)
     logger.info("Boost vae for distributed training")
+
+
+    discriminator, disc_optimizer, _, _, disc_lr_scheduler = booster.boost(
+        model=discriminator, optimizer=disc_optimizer, lr_scheduler=disc_lr_scheduler
+    )
+    logger.info("Boost discriminator for distributed training")
+
 
     # =======================================================
     # 6. training loop
@@ -191,6 +200,7 @@ def main():
     if cfg.load is not None:
         logger.info("Loading checkpoint")
         booster.load_model(vae, os.path.join(cfg.load, "model"))
+        booster.load_model(discriminator, os.path.join(cfg.load, "discriminator"))
         booster.load_optimizer(optimizer, os.path.join(cfg.load, "optimizer"))
         booster.load_optimizer(disc_optimizer, os.path.join(cfg.load, "disc_optimizer"))
         if lr_scheduler is not None:
@@ -206,11 +216,29 @@ def main():
 
     dataloader.sampler.set_start_index(sampler_start_idx)
 
-    # # define loss function
-    # loss_function = VEA3DLoss(kl_weight=cfg.kl_weight, perceptual_weight=cfg.perceptual_weight).to(device, dtype)
+    # 6.2 Define loss functions
+    nll_loss_fn = VEALoss(
+        perceptual_loss_weight = cfg.perceptual_loss_weight,
+        kl_loss_weight = cfg.kl_loss_weight,
+    )
 
+    adversarial_loss_fn = AdversarialLoss(
+        discriminator_factor = cfg.discriminator_factor,
+        discriminator_start = cfg.discriminator_start,
+    )
 
-    # 6.2. training loop
+    disc_loss_fn = DiscriminatorLoss(
+        discriminator_factor = cfg.discriminator_factor,
+        discriminator_start = cfg.discriminator_start,
+        discriminator_loss = cfg.discriminator_loss,
+    )   
+
+    # 6.3. training loop
+
+    # calculate discriminator_time_padding
+    disc_time_downsample_factor = 2 ** cfg.discriminator.discriminator_channel_multipliers
+    disc_time_padding = disc_time_downsample_factor - cfg.num_frames % disc_time_downsample_factor
+
     for epoch in range(start_epoch, cfg.epochs):
         dataloader.sampler.set_epoch(epoch)
         dataloader_iter = iter(dataloader)
@@ -241,37 +269,58 @@ def main():
                 else:
                     video = x
 
-                # loss = vae.get_loss(x)
-                loss, recon_video, log = vae(
-                    video,
-                    global_step,
-                    video_contains_first_frame = video_contains_first_frame,
-                    split = "train",
-                )
+                # padded videos for GAN
+                if global_step > cfg.discriminator_start:
+                    real_video = pad_at_dim(video, (disc_time_padding, 0), value = 0., dim = 2)
+                    fake_video = pad_at_dim(recon_video, (disc_time_padding, 0), value = 0., dim = 2)
 
-                # Backward & update
-                booster.backward(loss=loss, optimizer=optimizer)
-                optimizer.step()
+                #  ====== VAE ======
                 optimizer.zero_grad()
-
-                disc_loss, disc_log = vae.disc_forward(
+                recon_video, posterior = vae(
+                    video,
+                    video_contains_first_frame = video_contains_first_frame,
+                )
+                # simple nll loss
+                nll_loss, nll_loss_log = nll_loss_fn(
                     video,
                     recon_video,
-                    global_step,
-                    split = "train",
+                    posterior,
+                    split = "train"
                 )
-
+                vae_loss = nll_loss
+                # adversarial loss 
+                if global_step > cfg.discriminator_start:
+                    fake_logits = discriminator(fake_video.contiguous)
+                    adversarial_loss = adversarial_loss_fn(
+                        fake_logits,
+                        nll_loss, 
+                        vae.get_last_layer(),
+                        global_step,
+                        is_training = vae.training,
+                    )
+                    vae_loss += adversarial_loss
+                # Backward & update
+                booster.backward(loss=vae_loss, optimizer=optimizer)
+                optimizer.step()
+                # Log loss values:
+                all_reduce_mean(vae_loss)
+                running_loss += vae_loss.item()
+                
+                #  ====== Discriminator ======
+                disc_optimizer.zero_grad()
+                # if video_contains_first_frame:
+                # Since we don't have enough T frames, pad anyways
+                real_logits = discriminator(real_video.contiguous.detach())
+                fake_logits = discriminator(fake_video.contiguous.detach())
+                disc_loss = disc_loss_fn(real_logits, fake_logits, global_step)
+                # Backward & update
                 booster.backward(loss=disc_loss, optimizer=disc_optimizer)
                 disc_optimizer.step()
-                disc_optimizer.zero_grad()
-
                 # Log loss values:
-                all_reduce_mean(loss)
-                running_loss += loss.item()
                 all_reduce_mean(disc_loss)
                 running_disc_loss += disc_loss.item()
+            
                 log_step += 1
-
 
                 # Log to tensorboard
                 if coordinator.is_master() and (global_step + 1) % cfg.log_every == 0:
@@ -280,14 +329,15 @@ def main():
                     pbar.set_postfix({"loss": avg_loss, "disc_loss": avg_disc_loss, "step": step, "global_step": global_step})
                     running_loss = 0
                     log_step = 0
-                    writer.add_scalar("loss", loss.item(), global_step)
+                    writer.add_scalar("loss", vae_loss.item(), global_step)
                     if cfg.wandb:
                         wandb.log(
                             {
                                 "iter": global_step,
                                 "num_samples": global_step * total_batch_size,
                                 "epoch": epoch,
-                                "loss": loss.item(),
+                                "loss": vae_loss.item(),
+                                "disc_loss": disc_loss.item(),
                                 "avg_loss": avg_loss,
                             },
                             step=global_step,
@@ -297,8 +347,8 @@ def main():
                 if cfg.ckpt_every > 0 and (global_step + 1) % cfg.ckpt_every == 0:
                     save_dir = os.path.join(exp_dir, f"epoch{epoch}-global_step{global_step+1}")
                     os.makedirs(os.path.join(save_dir, "model"), exist_ok=True)
-                    # TODO: save in model?
                     booster.save_model(vae, os.path.join(save_dir, "model"), shard=True)
+                    booster.save_model(discriminator, os.path.join(save_dir, "discriminator"), shard=True)
                     booster.save_optimizer(optimizer, os.path.join(save_dir, "optimizer"), shard=True, size_per_shard=4096)
                     booster.save_optimizer(disc_optimizer, os.path.join(save_dir, "disc_optimizer"), shard=True, size_per_shard=4096)
 
