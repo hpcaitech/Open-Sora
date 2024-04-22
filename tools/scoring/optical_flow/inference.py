016f3fa46de27caa29fa721893e32e1bb572509a
xyupeng <99191637+xyupeng@users.noreply.github.com>
2024-04-22 17:15:55 +0800

    Dev/pxy (#61)
    * update scoring/matching
    
    * update scoring/matching
    
    * update scoring/matching
    
    * update scoring/matching
    
    * update scoring/matching
    
    * update scoring/matching
    
    * update scoring/matching
    
    * update scoring/matching
    
    * update scoring/matching
    
    * update scene_cut
    
    * update scene_cut
    
    * update scene_cut[A
    
    * update scene_cut
    
    * update scene_cut
    
    * update scene_cut
    
    * update scene_cut
    
    * update scene_cut
    
    * update scene_cut
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * update readme
    
    * update readme
    
    * extract frames using opencv everywhere
    
    * extract frames using opencv everywhere
    
    * extract frames using opencv everywhere
    
    * filter panda10m
    
    * filter panda10m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * m
    
    * ocr
    
    * add ocr
    
    * add main.sh
    
    * add ocr
    
    * add ocr
    
    * add ocr
    
    * add ocr
    
    * add ocr
    
    * add ocr
    
    * update scene_cut
    
    * update remove main.sh

--- tools/scoring/optical_flow/inference.py
+++ tools/scoring/optical_flow/inference.py
@@ -63,7 +63,7 @@ def main():
     torch.backends.cudnn.benchmark = False
     colossalai.launch_from_torch({})
     parser = argparse.ArgumentParser()
-    parser.add_argument("meta_path", type=str, help="Path to the input CSV file")
+    parser.add_argument("--meta_path", type=str, help="Path to the input CSV file")
     parser.add_argument("--bs", type=int, default=4, help="Batch size")
     parser.add_argument("--num_workers", type=int, default=16, help="Number of workers")
     args = parser.parse_args()
