1171e5b6f9ed67d88d7433e5d72ffbd8377943f4
zhengzangw <zhengzangw@gmail.com>
2024-04-29 07:27:15 +0000

    update config

--- configs/vae_magvit_v2/inference/17x128x128_pixabay.py
+++ configs/vae_magvit_v2/inference/17x128x128_pixabay.py
@@ -36,44 +36,44 @@ vae_2d = dict(
 
 model = dict(
     type="VAE_MAGVIT_V2",
-    in_out_channels = 4,
-    latent_embed_dim = 64,
-    filters = 128,
-    num_res_blocks = 4,
-    channel_multipliers = (1, 2, 2, 4),
-    temporal_downsample = (False, True, True),
-    num_groups = 32, # for nn.GroupNorm
-    kl_embed_dim = 4,
-    activation_fn = 'swish',
-    separate_first_frame_encoding = False,
-    disable_space = True,
-    custom_conv_padding = None,
-    encoder_double_z = True,
+    in_out_channels=4,
+    latent_embed_dim=64,
+    filters=128,
+    num_res_blocks=4,
+    channel_multipliers=(1, 2, 2, 4),
+    temporal_downsample=(False, True, True),
+    num_groups=32,  # for nn.GroupNorm
+    kl_embed_dim=4,
+    activation_fn="swish",
+    separate_first_frame_encoding=False,
+    disable_space=True,
+    custom_conv_padding=None,
+    encoder_double_z=True,
 )
 
 discriminator = dict(
     type="DISCRIMINATOR_3D",
-    image_size = (128, 128),
-    num_frames = num_frames,
-    in_channels = 3,
-    filters = 128,
-    channel_multipliers = (2,4,4,4,4),
+    image_size=(128, 128),
+    num_frames=num_frames,
+    in_channels=3,
+    filters=128,
+    channel_multipliers=(2, 4, 4, 4, 4),
     # channel_multipliers = (2,4,4), #(2,4,4,4,4) # (2,4,4,4) for 64x64 resolution
 )
 
 
-# loss weights 
-logvar_init=0.0
+# loss weights
+logvar_init = 0.0
 kl_loss_weight = 0.000001
-perceptual_loss_weight = 0.1 # use vgg is not None and more than 0
-discriminator_factor = 1.0 # for discriminator adversarial loss
+perceptual_loss_weight = 0.1  # use vgg is not None and more than 0
+discriminator_factor = 1.0  # for discriminator adversarial loss
 # discriminator_loss_weight = 0.5 # for generator adversarial loss
-generator_factor = 0.1 # for generator adversarial loss
-lecam_loss_weight = None # NOTE: not clear in MAGVIT what is the weight
-discriminator_loss_type="non-saturating"
-generator_loss_type="non-saturating"
-discriminator_start = 2500 # 50000 NOTE: change to correct val, debug use -1 for now
-gradient_penalty_loss_weight = None # 10 # SCH: MAGVIT uses 10, opensora plan doesn't use
+generator_factor = 0.1  # for generator adversarial loss
+lecam_loss_weight = None  # NOTE: not clear in MAGVIT what is the weight
+discriminator_loss_type = "non-saturating"
+generator_loss_type = "non-saturating"
+discriminator_start = 2500  # 50000 NOTE: change to correct val, debug use -1 for now
+gradient_penalty_loss_weight = None  # 10 # SCH: MAGVIT uses 10, opensora plan doesn't use
 ema_decay = 0.999  # ema decay factor for generator
 
 
@@ -83,11 +83,11 @@ save_dir = "outputs/samples_pixabay_17"
 wandb = False
 
 # Training
-''' NOTE: 
+""" NOTE:
 magvit uses about # samples (K) * epochs ~ 2-5 K,  num_frames = 4, reso = 128
-==> ours num_frams = 16, reso = 256, so samples (K) * epochs ~ [500 - 1200], 
+==> ours num_frams = 16, reso = 256, so samples (K) * epochs ~ [500 - 1200],
 3-6 epochs for pexel, from pexel observation its correct
-'''
+"""
 
 
 batch_size = 1
