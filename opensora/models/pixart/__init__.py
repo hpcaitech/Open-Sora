59591d8afcfe273b3a19d0ed57b43da1324f3317
周钰坤 <90625606+1zeryu@users.noreply.github.com>
2024-04-30 13:36:41 +0800

    update

--- opensora/models/pixart/__init__.py
+++ opensora/models/pixart/__init__.py
@@ -1,2 +1,2 @@
-from .pixart import PixArt, PixArt_XL_2
+from .pixart import PixArt, PixArt_XL_2, PixArt_1B_2
 from .pixart_sigma import PixArt_Sigma_XL_2
---
+++ train_pixart_1B.sh
@@ -0,0 +1,1 @@
+colossalai run --nproc_per_node 8 scripts/train.py configs/pixart/train/1x2048x2048.py --data-path /home/zhaowangbo/data/csv/image-v1_1_ext_noempty_rcp_clean_info.csv
