b2a236ab609c0e53e48c25d97a3607a33888c017
zhengzangw <zhengzangw@gmail.com>
2024-05-15 08:07:49 +0000

    [feat] accelerate search batch size

--- opensora/utils/misc.py
+++ opensora/utils/misc.py
@@ -135,6 +135,18 @@ def format_time(seconds):
     return f
 
 
+class BColors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
 # ======================================================
 # PyTorch
 # ======================================================
