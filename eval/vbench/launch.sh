ebacf114f37a17a36f445cdd3db3242387ba28d5
Shen Chenhui <e0052680@u.nus.edu>
2024-05-27 13:42:22 +0800

    update vbench launch to follow other launch scripts by using num_frames (currently set to 51) (#111)
    Co-authored-by: Shen-Chenhui <shen_chenhui@u.nus.edu>

--- eval/vbench/launch.sh
+++ eval/vbench/launch.sh
@@ -5,6 +5,7 @@ set -e
 
 CKPT=$1
 MODEL_NAME=$2
+NUM_FRAMES=51
 
 if [[ $CKPT == *"ema"* ]]; then
     parentdir=$(dirname $CKPT)
@@ -19,5 +20,5 @@ GPUS=(0 1 2 3 4 5 6 7)
 TASK_ID_LIST=(4a 4b 4c 4d 4e 4f 4g 4h)
 
 for i in "${!GPUS[@]}"; do
-    CUDA_VISIBLE_DEVICES=${GPUS[i]} bash eval/sample.sh $CKPT -${TASK_ID_LIST[i]} >${LOG_BASE}_${TASK_ID_LIST[i]}.log 2>&1 &
+    CUDA_VISIBLE_DEVICES=${GPUS[i]} bash eval/sample.sh $CKPT ${NUM_FRAMES} ${MODEL_NAME} -${TASK_ID_LIST[i]} >${LOG_BASE}_${TASK_ID_LIST[i]}.log 2>&1 &
 done
