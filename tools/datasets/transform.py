bb835944b4bc4d8c1a9d130e47ef8e3146d599e5
Shen-Chenhui <shen_chenhui@u.nus.edu>
2024-06-10 07:41:11 +0000

    format

--- tools/datasets/transform.py
+++ tools/datasets/transform.py
@@ -37,16 +37,19 @@ def resize(path, length, input_dir, output):
     ext = os.path.splitext(path)[1].lower()
     assert ext in IMG_EXTENSIONS
     img = cv2.imread(path)
-    h, w = img.shape[:2]
-    if min(h, w) > length:
-        if h > w:
-            new_h = length
-            new_w = int(w * new_h / h)
-        else:
-            new_w = length
-            new_h = int(h * new_w / w)
-        img = cv2.resize(img, (new_w, new_h))
-    cv2.imwrite(path_new, img)
+    if img is not None:
+        h, w = img.shape[:2]
+        if min(h, w) > length:
+            if h > w:
+                new_h = length
+                new_w = int(w * new_h / h)
+            else:
+                new_w = length
+                new_h = int(h * new_w / w)
+            img = cv2.resize(img, (new_w, new_h))
+        cv2.imwrite(path_new, img)
+    else:
+        path_new = ""
     return path_new
 
 
@@ -55,18 +58,21 @@ def rand_crop(path, input_dir, output):
     path_new = get_new_path(path, input_dir, output)
     assert ext in IMG_EXTENSIONS
     img = cv2.imread(path)
-    h, w = img.shape[:2]
-    width, height, _ = img.shape
-    pos = random.randint(0, 3)
-    if pos == 0:
-        img_cropped = img[: width // 2, : height // 2]
-    elif pos == 1:
-        img_cropped = img[width // 2 :, : height // 2]
-    elif pos == 2:
-        img_cropped = img[: width // 2, height // 2 :]
+    if img is not None:
+        h, w = img.shape[:2]
+        width, height, _ = img.shape
+        pos = random.randint(0, 3)
+        if pos == 0:
+            img_cropped = img[: width // 2, : height // 2]
+        elif pos == 1:
+            img_cropped = img[width // 2 :, : height // 2]
+        elif pos == 2:
+            img_cropped = img[: width // 2, height // 2 :]
+        else:
+            img_cropped = img[width // 2 :, height // 2 :]
+        cv2.imwrite(path_new, img_cropped)
     else:
-        img_cropped = img[width // 2 :, height // 2 :]
-    cv2.imwrite(path_new, img_cropped)
+        path_new = ""
     return path_new
 
 
@@ -74,8 +80,10 @@ def main(args):
     data = pd.read_csv(args.input)
     if args.method == "img_rand_crop":
         data["path"] = apply(data["path"], lambda x: rand_crop(x, args.input_dir, args.output))
+        output_csv = args.input.replace(".csv", f"_rand_crop.csv")
     elif args.method == "img_resize":
         data["path"] = apply(data["path"], lambda x: resize(x, args.length, args.input_dir, args.output))
+        output_csv = args.input.replace(".csv", f"_resized{args.length}.csv")
     elif args.method == "vid_frame_extract":
         points = args.points if args.points is not None else args.points_index
         data = pd.DataFrame(np.repeat(data.values, 3, axis=0), columns=data.columns)
@@ -87,8 +95,8 @@ def main(args):
             else:
                 data.loc[i::num_points, "point"] = data.loc[i::num_points, "num_frames"] * point
         data["path"] = apply(data, lambda x: extract_frames(x["path"], args.input_dir, args.output, x["point"]), axis=1)
+        output_csv = args.input.replace(".csv", f"_vid_frame_extract.csv")
 
-    output_csv = args.input.replace(".csv", f"_resized{args.length}.csv")
     data.to_csv(output_csv, index=False)
     print(f"Saved to {output_csv}")
 
