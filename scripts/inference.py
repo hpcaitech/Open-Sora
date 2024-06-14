50ef1fecefa8eb9be3f4a89a956c4ddd61988d0a
zhengzangw <zhengzangw@gmail.com>
2024-06-14 17:36:11 +0000

    minor update

--- scripts/inference.py
+++ scripts/inference.py
@@ -147,7 +147,6 @@ def main():
 
         # == get json from prompts ==
         batch_prompts, refs, ms = extract_json_from_prompts(batch_prompts, refs, ms)
-        original_prompts = batch_prompts
 
         # == get reference for condition ==
         refs = collect_references_batch(refs, vae, image_size)
@@ -165,7 +164,7 @@ def main():
                     save_dir,
                     sample_name=sample_name,
                     sample_idx=start_idx + idx,
-                    prompt=original_prompts[idx],
+                    prompt=batch_prompts[idx],
                     prompt_as_path=prompt_as_path,
                     num_sample=num_sample,
                     k=k,
