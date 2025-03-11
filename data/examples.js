fe4d1f5b84b4bdfae8794097e49a2226c4fe3aa5
wuxiwen <wuxiwen.simon@gmail.com>
2025-03-11 17:01:42 +0800

    update gallery

--- data/examples.js
+++ data/examples.js
@@ -7,15 +7,63 @@ const examples = [
           "A group of anthropomorphic mushrooms having a disco party in the middle of a dark enchanted forest, with glowing neon lights and exaggerated dance moves, their smooth textures and reflective surfaces emphasizing a comical 3D look.",
         inputs: [],
         output: {
-          link: "https://streamable.com/8g9y9h",
+          link: "https://streamable.com/e/8g9y9h?autoplay=1",
         },
       },
       {
         prompt:
-          "A playful dog in a pink coat with a red leash dashes across a muddy field with sparse crops. The camera tracks its energetic movement from right to left against a backdrop of trees and distant power lines under an overcast sky. The realistic, medium shot captures a candid, lively moment in soft, diffused light.",
+          "A panda bear with distinct black patches climbs and rests on a wooden log platform amid lush, natural foliage.",
         inputs: [],
         output: {
-          link: "https://streamable.com/lsoai1",
+          link: "https://streamable.com/e/3wif07?autoplay=1",
+        },
+      },
+      {
+        prompt:
+          "A vibrant green parrot with hints of yellow and blue perches on a person’s lap, who is wearing grey pants. The parrot features a white beak, grey head, and a black eye. In the background, a red couch and a TV displaying a colorful video with “bilibili” text complete the scene.",
+        inputs: [],
+        output: {
+          link: "https://streamable.com/e/cljw09?autoplay=1",
+        },
+      },
+      {
+        prompt:
+          "A black-and-white film captures a pianist playing in an empty, decaying theater. His deft fingers and echoing music create a haunting ambiance as dust motes float in the faint light. The gleaming grand piano under the spotlight contrasts with the worn seats and peeling walls, evoking nostalgia.",
+        inputs: [],
+        output: {
+          link: "https://streamable.com/e/sc2hco?autoplay=1",
+        },
+      },
+      {
+        prompt:
+          "Chinese ancient style, realism. A young woman, dressed in an embroidered red qipao, walks along the ancient streets of a bustling Chinese town. The red lanterns hanging above her sway gently in the evening breeze, and her calm, confident stride contrasts with the lively atmosphere of merchants and performers around her. ",
+        inputs: [],
+        output: {
+          link: "https://streamable.com/e/dsv8da?autoplay=1",
+        },
+      },
+      {
+        prompt:
+          "A tomato surfing on a piece of lettuce down a waterfall of ranch dressing, with exaggerated surfing moves and creamy wave effects to highlight the 3D animated fun. ",
+        inputs: [],
+        output: {
+          link: "https://streamable.com/e/28krjb?autoplay=1",
+        },
+      },
+      {
+        prompt:
+          "A man in a gray hoodie and a woman in a light gray jacket jog along a residential sidewalk, smiling and chatting. They pass a beige house with a vibrant garden and street lamp on a bright, sunny day. The medium shot captures their movement amid lush greenery, creating a serene, cinematic scene.",
+        inputs: [],
+        output: {
+          link: "https://streamable.com/e/bzrn9n?autoplay=1",
+        },
+      },
+      {
+        prompt:
+          "A coastal landscape painting with a prominent archway is displayed on an easel in a bright studio. A camera pan reveals a table cluttered with art supplies and a potted plant, enhancing the artistic vibe. Large windows and soft natural lighting create a cozy, creative atmosphere.",
+        inputs: [],
+        output: {
+          link: "https://streamable.com/e/voisj4?autoplay=1",
         },
       },
       {
@@ -23,55 +71,63 @@ const examples = [
           "A scene from disaster movie.",
         inputs: [],
         output: {
-          link: "https://streamable.com/yfwk8i",
+          link: "https://streamable.com/e/yfwk8i?autoplay=1",
         },
       },
       {
         prompt:
-          "A man performs push-ups on a wooden bench in a sunny park, captured from a side angle in a medium shot. The focus is on his upper body and technique, with natural sunlight accentuating the scene. Lush greenery and distant park-goers contribute to the energetic, realistic setting.",
+          "A candid medium shot captures a woman in a white car, wearing glasses, a yellow top, and a black jacket, with her arm resting on the open window. Behind her, a stone-faced house surrounded by lush greenery basks in natural sunlight, creating a warm and realistic scene.",
         inputs: [],
         output: {
-          link: "https://streamable.com/k50mnv",
+          link: "https://streamable.com/e/us2w7h?autoplay=1",
         },
       },
       {
         prompt:
-          "A medium shot captures a woman in a white car, wearing glasses, a yellow top, and a black jacket, with her arm resting on the open window. The background shows a stone-facade house amid lush greenery and natural sunlight filtering through trees. The realistic scene exudes a bright, warm, and candid atmosphere.",
+          "Two women sit on a beige couch in a cozy, warmly lit room with a brick wall backdrop. They engage in a cheerful conversation, smiling and toasting red wine in an intimate medium shot.",
         inputs: [],
         output: {
-          link: "https://streamable.com/us2w7h",
+          link: "https://streamable.com/e/qgw5oz?autoplay=1",
         },
       },
       {
         prompt:
-          "A drone captures a historic church perched on a rocky outcropping along Italy's Amalfi Coast, highlighting its intricate architecture and tiered patios. The view features crashing waves, expansive coastal waters, and distant figures enjoying the dramatic vistas. Bathed in the warm glow of the afternoon sun, the scene exudes a magical, romantic ambiance.",
+          "A woman with her hair in a bun walks along a city sidewalk, gently touching a lush hedge. Dressed in a plaid jacket and beige pants with a tan backpack, her calm presence is captured in natural daylight against an urban backdrop.",
         inputs: [],
         output: {
-          link: "https://streamable.com/jgjil0",
+          link: "https://streamable.com/e/88y66s?autoplay=1",
         },
       },
       {
         prompt:
-          "A panda bear climbed and rested on a wooden platform constructed from logs. It is predominantly white with black patches around its eyes, ears, and body. The surrounding environment is lush with green foliage, indicating a natural habitat.",
+          "A breathtaking aerial view shows a river winding like a dark ribbon through lush fields and hills, reflecting the soft pink-orange hues of sunrise or sunset in a serene, picturesque landscape.",
         inputs: [],
         output: {
-          link: "https://streamable.com/3wif07",
+          link: "https://streamable.com/e/yb5ypw?autoplay=1",
         },
       },
       {
         prompt:
-          "A tomato surfing on a piece of lettuce down a waterfall of ranch dressing, with exaggerated surfing moves and creamy wave effects to highlight the 3D animated fun.",
+          "A man performs push-ups on a wooden bench in a sunny park, captured from a side angle in a medium shot. The focus is on his upper body and technique, with natural sunlight accentuating the scene. Lush greenery and distant park-goers contribute to the energetic, realistic setting",
         inputs: [],
         output: {
-          link: "https://streamable.com/28krjb",
+          link: "https://streamable.com/e/k50mnv?autoplay=1",
         },
       },
       {
         prompt:
-          "A man and a woman jog on a residential sidewalk on a bright, sunny day, smiling and conversing. The man wears a gray hoodie over a red shirt, and the woman sports a light gray jacket with blue pants. A medium shot captures their movement past a beige house with a vibrant garden and street lamp, set in a serene environment.",
+          "A playful dog in a pink coat with a red leash dashes across a muddy field with sparse crops. The camera tracks its energetic movement from right to left against a backdrop of trees and distant power lines under an overcast sky. The realistic, medium shot captures a candid, lively moment in soft, diffused light.",
         inputs: [],
         output: {
-          link: "https://streamable.com/bzrn9n",
+          link: "https://streamable.com/e/lsoai1?autoplay=1",
+        },
+      },
+      {
+        prompt:
+          "A drone camera circles a historic church on a rocky outcrop along the Amalfi Coast, highlighting its stunning architecture, tiered patios, and the dramatic coastal views with waves crashing below and people enjoying the scene in the warm afternoon light.",
+        inputs: [],
+        output: {
+          link: "https://streamable.com/e/jgjil0?autoplay=1",
         },
       },
     ],
